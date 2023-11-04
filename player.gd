b0262c090cbdbaa8aba4395de2981bc2e5a5d7f4
lokihowellstudios <lokihowellstudios@gmail.com>
2023-11-03 20:02:23 -0400

    Update player.gd
    fixed

--- player.gd
+++ player.gd
@@ -11,8 +11,8 @@ func _physics_process(delta):
 	player_movement(delta)
 
 func get_input():
-	input.x = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
-	input.y = int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed(("move_up"))
+	input.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	input.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
 	return input.normalized()
 
 func player_movement(delta):
@@ -20,11 +20,11 @@ func player_movement(delta):
 	
 	if input == Vector2.ZERO:
 		if velocity.length() > (friction * delta):
-			velocity -= velocity.normalized() + (friction * delta)
+			velocity -= velocity.normalized() * (friction * delta)
 		else:
 			velocity = Vector2.ZERO
 	else:
-		velocity += (input + accel + delta)
+		velocity += (input * accel * delta)
 		velocity = velocity.limit_length(max_speed)
 		
 	
