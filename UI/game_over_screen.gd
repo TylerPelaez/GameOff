96752b427c1c185a31b5590517ce1029e46d65ee
Tyler Pelaez <mail@tylerpelaez.com>
2023-12-01 16:02:18 -0500

    stuff

--- UI/game_over_screen.gd
+++ UI/game_over_screen.gd
@@ -11,3 +11,7 @@ func _on_restart_button_pressed():
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+
+func _on_menu_pressed():
+		get_tree().change_scene_to_file("res://UI/main_menu.tscn")
